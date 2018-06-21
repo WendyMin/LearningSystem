391bf0bc7aede46a5877972627c696ea53117876
WendyMin <min711s9d31@126.com>
2018-06-21 14:02:24 +0800

    +readme

---
+++ Frontend/public/static/js/8.chunk-08bc4d14b46ae15fb12c.js
@@ -0,0 +1,6 @@
+/*!
+ * Direct.js
+ * Antinux Innovation
+ * Author: Eric Deng
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{587:function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var u=s(o(2)),c=s(o(1)),t=s(o(574)),l=o(147);function s(e){return e&&e.__esModule?e:{default:e}}n.default=(0,t.default)(function(e){return c.default.createElement(l.view,(0,u.default)({},e,{onCancel:function(){e.onCancel(),e.closeWindow()},onSuccess:function(){e.onSuccess(),e.closeWindow()}}))})}}]);
