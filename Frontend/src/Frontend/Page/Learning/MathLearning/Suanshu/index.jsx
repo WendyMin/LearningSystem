9347860799bba4e25c5edc9ee167b39e791e18b3
unknown <2211494516@qq.com>
2018-09-26 11:10:29 +0800

    +msg

--- Frontend/src/Frontend/Page/Learning/MathLearning/Suanshu/index.jsx
+++ Frontend/src/Frontend/Page/Learning/MathLearning/Suanshu/index.jsx
@@ -5,6 +5,7 @@ import { Prompt } from 'react-router';
 import style from 'style';
 
 import UserManagerWindow from "Windows/UserManager";
+import MathKnowledge from 'Page/Learning/MathLearning/MathKnowledge';
 
 import protect from 'direct-core/protect';
 import asyncProcessControl from 'direct-core/asyncProcessControl';
@@ -19,6 +20,10 @@ import { actions as SubjectFunctionSelectActions } from 'Connected/SubjectFuncti
 class Suanshu extends React.PureComponent {
   constructor( props ){
     super( props );
+    this.state = {
+      showChapter: true,
+      showKnowledge: false
+    }
     const {
       setLearningType,
       learningType,
@@ -34,78 +39,91 @@ class Suanshu extends React.PureComponent {
 
     return(
       <React.Fragment>
+        {this.state.showChapter ?
+        <div>
+          <div className="col-md-4" >
+            <div className="card-box kanban-box">
+              <div className="kanban-detail">
+                  <span className="label label-primary pull-right">Unfinish</span>
+                  <div className={style.title18}>
+                    第一章
+                  </div>
+                  <ul className="list-inline m-b-0">
+                      <li>
+                          <br/>
+                          <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5"
+                                  onClick = {() => this.setState({showChapter: false , showKnowledge: true})}
+                            >
+                            知识点1</button>
+                          <br/>
+                          <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
+                            知识点2</button>
+                          <br/>
+                          <button className="btn btn-primary btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-primary btn-trans waves-effect waves-primary w-md m-b-5" >
+                            知识点3</button>
+                      </li>
+                  </ul>
+              </div>
+            </div>
+          </div>
 
-        <div className="col-md-4" >
-          <div className="card-box kanban-box">
-            <div className="kanban-detail">
-                <span className="label label-primary pull-right">Unfinish</span>
-                <div className={style.title18}>
-                  第一章
-                </div>
-                <ul className="list-inline m-b-0">
-                    <li>
-                        <br/>
-                        <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
-                          知识点1</button>
-                        <br/>
-                        <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
-                          知识点2</button>
-                        <br/>
-                        <button className="btn btn-primary btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-primary btn-trans waves-effect waves-primary w-md m-b-5" >
-                          知识点3</button>
-                    </li>
-                </ul>
+          <div className="col-md-4" >
+            <div className="card-box kanban-box">
+              <div className="kanban-detail">
+                  <span className="label label-primary pull-right">Unfinish</span>
+                  <div className={style.title18}>
+                    第二章
+                  </div>
+                  <ul className="list-inline m-b-0">
+                      <li>
+                          <br/>
+                          <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
+                            知识点1</button>
+                          <br/>
+                          <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
+                            知识点2</button>
+                          <br/>
+                          <button className="btn btn-primary btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-primary btn-trans waves-effect waves-primary w-md m-b-5" >
+                            知识点3</button>
+                      </li>
+                  </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4" >
+            <div className="card-box kanban-box">
+              <div className="kanban-detail">
+                  <span className="label label-primary pull-right">Unfinish</span>
+                  <div className={style.title18}>
+                    第三章
+                  </div>
+                  <ul className="list-inline m-b-0">
+                      <li>
+                          <br/>
+                          <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
+                            知识点1</button>
+                          <br/>
+                          <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
+                            知识点2</button>
+                          <br/>
+                          <button className="btn btn-primary btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-primary btn-trans waves-effect waves-primary w-md m-b-5" >
+                            知识点3</button>
+                      </li>
+                  </ul>
+              </div>
             </div>
           </div>
         </div>
+        :
+        this.state.showKnowledge ?
+        <MathKnowledge chapterNum = {this.chapterNum} knowledgeNum = {this.knowledgeNum}/>
+        :
+        null
 
-        <div className="col-md-4" >
-          <div className="card-box kanban-box">
-            <div className="kanban-detail">
-                <span className="label label-primary pull-right">Unfinish</span>
-                <div className={style.title18}>
-                  第二章
-                </div>
-                <ul className="list-inline m-b-0">
-                    <li>
-                        <br/>
-                        <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
-                          知识点1</button>
-                        <br/>
-                        <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
-                          知识点2</button>
-                        <br/>
-                        <button className="btn btn-primary btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-primary btn-trans waves-effect waves-primary w-md m-b-5" >
-                          知识点3</button>
-                    </li>
-                </ul>
-            </div>
-          </div>
-        </div>
+        }
 
-        <div className="col-md-4" >
-          <div className="card-box kanban-box">
-            <div className="kanban-detail">
-                <span className="label label-primary pull-right">Unfinish</span>
-                <div className={style.title18}>
-                  第三章
-                </div>
-                <ul className="list-inline m-b-0">
-                    <li>
-                        <br/>
-                        <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
-                          知识点1</button>
-                        <br/>
-                        <button className="btn btn-success btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-success btn-trans waves-effect w-md waves-success m-b-5" >
-                          知识点2</button>
-                        <br/>
-                        <button className="btn btn-primary btn-sm waves-effect waves-primary w-md waves-success m-b-5 btn btn-primary btn-trans waves-effect waves-primary w-md m-b-5" >
-                          知识点3</button>
-                    </li>
-                </ul>
-            </div>
-          </div>
-        </div>
+
 
 
       </React.Fragment>
